09cd929a2a447ce4dea7cc00f3857e2325f0dcff
Chaitanya Vaddisriram <53746340+chaitanyavaddi@users.noreply.github.com>
2025-06-20 16:13:55 +0530

    setupa

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from src.api import router as app_router
 
 app = FastAPI(title="Medha AI", description="AI Mock Interview Service")
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,3 +12,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(app_router)
